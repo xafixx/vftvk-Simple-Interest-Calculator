1c9da3c94314ccc34ae79cf4e475d2e5b06dd7fc
Fikri Aqsha <m.fikriaqsha@gmail.com>
2022-02-12 14:25:10 +0700

    modified index.html, script.js, style.css

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+// update rate in interest rate value
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
 }
-        
+
+// run compute() function when the button clicked
+function compute() {
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+    p = document.getElementById("principal").value;
+    if (p <= 0) {
+        alert("Enter a positive number");
+        p.focus();
+        return false;
+    }
+    document.getElementById("result").innerHTML = "If you deposit " + "<mark>" + principal + "</mark>" + ",\<br\>at an interest rate of " + "<mark>" + rate + "%</mark>" + ".\<br\>You will receive an amount of " + "<mark>" + interest + "</mark>" + ",\<br\>in the year " + "<mark>" + year + "</mark>" + "\<br\>"
+}
